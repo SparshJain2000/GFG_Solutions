5e67e27347e07c2041d6eedad821e91c4f749f1e
SparshJain2000 <jainsparsh0801@gmail.com>
2021-02-05 01:11:02 +0530

    Stack : queue using 2 stack

---
+++ Stack and Queue/queue-using-two-stacks.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class StackQueue {
+   private:
+    stack<int> s1;
+    stack<int> s2;
+
+   public:
+    void push(int B);
+    int pop();
+};
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        StackQueue *sq = new StackQueue();
+
+        int Q;
+        cin >> Q;
+        while (Q--) {
+            int QueryType = 0;
+            cin >> QueryType;
+            if (QueryType == 1) {
+                int a;
+                cin >> a;
+                sq->push(a);
+            } else if (QueryType == 2) {
+                cout << sq->pop() << " ";
+            }
+        }
+        cout << endl;
+    }
+}
+
+void StackQueue ::push(int x) {
+    s1.push(x);
+}
+
+int StackQueue ::pop() {
+    if (s1.empty()) return -1;
+    while (s1.size() > 1) s2.push(s1.top()), s1.pop();
+    int temp = s1.top();
+    s1.pop();
+    while (!s2.empty())
+        s1.push(s2.top()), s2.pop();
+    return temp;
+}
