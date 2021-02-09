daa8163723f34abbe1d82aa4b5090eddf6420907
SparshJain2000 <jainsparsh0801@gmail.com>
2021-02-10 01:51:08 +0530

    Linked list : check palindrome

---
+++ Linked-List/check-if-linked-list-is-pallindrome.cpp
@@ -0,0 +1,69 @@
+// { Driver Code Starts
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <iostream>
+#include <stack>
+using namespace std;
+/* Link list Node */
+struct Node {
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+void append(struct Node **head_ref, struct Node **tail_ref, int new_data) {
+    struct Node *new_node = new Node(new_data);
+
+    if (*head_ref == NULL)
+        *head_ref = new_node;
+    else
+        (*tail_ref)->next = new_node;
+    *tail_ref = new_node;
+}
+
+bool isPalindrome(Node *head);
+/* Driver program to test above function*/
+int main() {
+    int T, i, n, l;
+    cin >> T;
+    while (T--) {
+        struct Node *head = NULL, *tail = NULL;
+        cin >> n;
+        for (i = 1; i <= n; i++) {
+            cin >> l;
+            append(&head, &tail, l);
+        }
+        cout << isPalindrome(head) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+/*
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+/*You are required to complete this method */
+#include <stack>
+bool isPalindrome(Node *head) {
+    Node *temp = head;
+    stack<int> s;
+    while (temp) s.push(temp->data), temp = temp->next;
+    temp = head;
+    while (!s.empty() && temp) {
+        if (s.top() != temp->data) return false;
+        s.pop();
+        temp = temp->next;
+    }
+    return true;
+}
