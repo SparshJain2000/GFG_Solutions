3f3e045425385cfe4dca22e6e3f04a10414bbce2
SparshJain2000 <jainsparsh0801@gmail.com>
2021-02-15 00:59:54 +0530

    Trees : left view of tree

---
+++ Trees/left-view-of-BT.cpp
@@ -0,0 +1,91 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// Tree Node
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+
+void leftView(struct Node* root);
+//{ DRIVER CODE
+// Utility function to create a new Tree Node
+Node* newNode(int val) {
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+
+// Function to Build Tree
+Node* buildTree(string str) {
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;)
+        ip.push_back(str);
+    Node* root = newNode(stoi(ip[0]));
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+        string currVal = ip[i];
+        if (currVal != "N") {
+            currNode->left = newNode(stoi(currVal));
+            queue.push(currNode->left);
+        }
+        i++;
+        if (i >= ip.size())
+            break;
+        currVal = ip[i];
+        if (currVal != "N") {
+            currNode->right = newNode(stoi(currVal));
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+int main() {
+    int t;
+    scanf("%d ", &t);
+    while (t--) {
+        string s;
+        getline(cin, s);
+        Node* root = buildTree(s);
+        leftView(root);
+        cout << endl;
+    }
+    return 0;
+}
+// DRIVER CODE ENDS
+
+// A wrapper over leftViewUtil()
+void leftView(Node* root) {
+    if (!root) return;
+    queue<Node*> q;
+    q.push(root);
+    while (!q.empty()) {
+        int n = q.size();
+        cout << q.front()->data << " ";
+        while (n--) {
+            Node* temp = q.front();
+            q.pop();
+            if (temp->left) q.push(temp->left);
+            if (temp->right) q.push(temp->right);
+        }
+    }
+}
