e8ae9fdfb09281bd44ca8d803122d2c497ca68e8
SparshJain2000 <jainsparsh0801@gmail.com>
2021-07-12 02:33:12 +0530

    ARRAYS : add method for kth element 2 sorted

--- Arrays/k-th-element-of-two-sorted-array.cpp
+++ Arrays/k-th-element-of-two-sorted-array.cpp
@@ -6,12 +6,30 @@ using namespace std;
 class Solution {
    public:
     int kthElement(int arr1[], int arr2[], int n, int m, int k) {
+        //O(nlogn + mlogm) time O(1) space
         for (int i = 0; i < min(n, m); i++)
             if (arr1[n - i - 1] > arr2[i]) swap(arr1[n - i - 1], arr2[i]);
         sort(arr1, arr1 + n);
         sort(arr2, arr2 + m);
         if (k <= n) return arr1[k - 1];
         return arr2[k - n - 1];
+
+        //O(k) time O(1) space
+        int i = 0, j = 0;
+        int ans = 0;
+        while (i < n && j < m && --k) {
+            if (arr1[i] < arr2[j])
+                i++;
+            else
+                j++;
+            ans = min(arr1[i], arr2[j]);
+        }
+        while (k--) {
+            if (i < n) ans = arr1[i++];
+            if (j < m) ans = arr2[j++];
+        }
+
+        return ans;
     }
 };
 
