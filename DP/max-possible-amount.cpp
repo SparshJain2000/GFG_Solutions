e505aac9b81a24ceaa882b350a63de16e2cbc13b
SparshJain2000 <jainsparsh0801@gmail.com>
2021-07-14 02:32:56 +0530

    DP : max possible amt

--- DP/max-possible-amount.cpp
+++ DP/max-possible-amount.cpp
@@ -2,46 +2,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution{
-	public:
-	int util(int arr[],int st,int end){
-	    if(st>end) return 0;
-	    if(st==end) return arr[st];
-	    if(dp[st][end]!=-1) return dp[st][end];
-	    dp[st][end]=max(min(util(arr,st+2,end),util(arr,st+1,end-1))+arr[st],arr[end]+min(util(arr,st,end-2),util(arr,st+1,end-1)));
-	    return dp[st][end];
-	}
-	vector<vector<int>>dp;
-	int maxAmount(int arr[], int n)
-	{
-		dp=vector<vector<int>>(n+1,vector<int>(n+1,-1));
-        return util(arr,0,n-1);
-	}
+class Solution {
+   public:
+    int util(int arr[], int st, int end) {
+        if (st > end) return 0;
+        if (st == end) return arr[st];
+        if (dp[st][end] != -1) return dp[st][end];
+        dp[st][end] = max(
+            min(util(arr, st + 2, end), util(arr, st + 1, end - 1)) + arr[st],
+            min(util(arr, st, end - 2), util(arr, st + 1, end - 1)) + arr[end]);
+        return dp[st][end];
+    }
+    vector<vector<int>> dp;
+    int maxAmount(int arr[], int n) {
+        dp = vector<vector<int>>(n + 1, vector<int>(n + 1, -1));
+        return util(arr, 0, n - 1);
+    }
 };
 
 // { Driver Code Starts.
-int main() 
-{
-   
-   
-   	int t;
+int main() {
+    int t;
     cin >> t;
-    while (t--)
-    {
+    while (t--) {
         int n;
         cin >> n;
 
         int a[n];
-        for(int i = 0; i < n; i++)
-        	cin >> a[i];
+        for (int i = 0; i < n; i++)
+            cin >> a[i];
 
-	   
-
-	    Solution ob;
-	    cout << ob.maxAmount(a, n) << "\n";
-
-	     
+        Solution ob;
+        cout << ob.maxAmount(a, n) << "\n";
     }
     return 0;
 }
-  // } Driver Code Ends
+// } Driver Code Ends
