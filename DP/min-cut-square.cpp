3a938ed8b60a438886a4bc610831d5e63d520709
SparshJain2000 <jainsparsh0801@gmail.com>
2021-07-15 01:16:17 +0530

    DP : min cut square

--- DP/min-cut-square.cpp
+++ DP/min-cut-square.cpp
@@ -2,28 +2,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
- // } Driver Code Ends
-class Solution{
-
-	public:
-	int minCut(int M, int N)
-	{
+// } Driver Code Ends
+class Solution {
+   public:
+    int minCut(int M, int N) {
         // Your code goes here
-        vector<vector<int>>dp(M+1,vector<int>(N+1,0));
-        for(int i=1;i<=N;i++) dp[1][i]=i;
-        for(int i=1;i<=M;i++) dp[i][1]=i;
-        
-        for(int i=2;i<=M;i++){
-            for(int j=2;j<=N;j++){
-                int x=INT_MAX;
-                if(i==j) x=1;
-                else{
-                    for(int k=i-1;k>=i/2;k--)
-                        x=min(dp[i-k][j]+dp[k][j],x);
-                    for(int k=j-1;k>=j/2;k--)
-                        x=min(dp[i][j-k]+dp[i][k],x);
+        vector<vector<int>> dp(M + 1, vector<int>(N + 1, 0));
+        for (int i = 1; i <= N; i++) dp[1][i] = i;
+        for (int i = 1; i <= M; i++) dp[i][1] = i;
+
+        for (int i = 2; i <= M; i++) {
+            for (int j = 2; j <= N; j++) {
+                int x = INT_MAX;
+                if (i == j)
+                    x = 1;
+                else {
+                    for (int k = i - 1; k >= i / 2; k--)
+                        x = min(dp[i - k][j] + dp[k][j], x);
+                    for (int k = j - 1; k >= j / 2; k--)
+                        x = min(dp[i][j - k] + dp[i][k], x);
                 }
-                dp[i][j]=x;
+                dp[i][j] = x;
                 // else{
                 //     if(i<j) dp[i][j]=dp[i][i]+dp[i][j-i];
                 //     else dp[i][j]=dp[j][j]+dp[i-j][j];
@@ -31,28 +30,20 @@ class Solution{
             }
         }
         return dp[M][N];
-	}
+    }
 };
 
 // { Driver Code Starts.
-int main() 
-{
-   	
-   
-   	int t;
+int main() {
+    int t;
     cin >> t;
-    while (t--)
-    {
+    while (t--) {
         int m, n;
         cin >> m >> n;
 
-	    
-
-	    Solution ob;
-	    cout << ob.minCut(m, n) << "\n";
-
-	     
+        Solution ob;
+        cout << ob.minCut(m, n) << "\n";
     }
     return 0;
 }
-  // } Driver Code Ends
+// } Driver Code Ends
