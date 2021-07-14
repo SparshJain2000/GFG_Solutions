77f83199eb8ac240ad1885f52a864e68626bff41
SparshJain2000 <jainsparsh0801@gmail.com>
2021-07-15 01:16:04 +0530

    DP : form a palindrome

---
+++ DP/form-a-palindrome.cpp
@@ -0,0 +1,40 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+   public:
+    int countMin(string s) {
+        // code here
+        string p = s;
+        reverse(p.begin(), p.end());
+        int n = s.length();
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+        // ans= LCS(s,reverse(s))
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (s[i - 1] == p[j - 1])
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                else
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+        return n - dp[n][n];
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string str;
+        cin >> str;
+        Solution ob;
+        cout << ob.countMin(str) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
