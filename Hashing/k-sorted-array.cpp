05091cc3c8fbebc11192624caaff7f2c2b44b808
SparshJain2000 <jainsparsh0801@gmail.com>
2021-08-24 00:19:50 +0530

    HASH : k-sorted array

---
+++ Hashing/k-sorted-array.cpp
@@ -0,0 +1,37 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution {
+   public:
+    string isKSortedArray(int arr[], int n, int k) {
+        map<int, int> mp;
+        for (int i = 0; i < n; i++) mp[arr[i]] = i;
+        int i = 0;
+        for (auto p : mp) {
+            if (abs(p.second - i) > k) return "No";
+            i++;
+        }
+        return "Yes";
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++)
+            cin >> arr[i];
+        cin >> k;
+        Solution ob;
+        cout << ob.isKSortedArray(arr, n, k) << endl;
+    }
+    return 0;
+}   // } Driver Code Ends
